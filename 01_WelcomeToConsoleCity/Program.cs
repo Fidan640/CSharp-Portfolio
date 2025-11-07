e2bebd4fc4fe938075a9f41851bf40c4cbb6a4c2
Fidan <fdashdamirova640@gmail.com>
2025-11-07 18:31:32 +0400

    Add Chapter 1: Welcome To Console City

---
+++ 01_WelcomeToConsoleCity/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WelcomeToConsoleCity
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Welcome message for Los Console City
+      Console.WriteLine("==================================");
+      Console.WriteLine("WELCOME TO LOS CONSOLE CITY! 🌉🚦");
+      Console.WriteLine("==================================");
+
+    }
+  }
+}
---
+++ 01_WelcomeToConsoleCity/README.md
@@ -0,0 +1,9 @@
+# 01 - Welcome to Console City
+Welcome message for Los Console City.
+
+**Key Concepts:**
+- C# Syntax
+- Console Output
+- Comments
+
+![Console Output](screenshot.png)
