7d3c47c5759e2cc61d7a6229fc1068f5c2a13f59
Fidan <fdashdamirova640@gmail.com>
2025-11-10 15:38:33 +0400

    Add Chapter 2: Player Profile

---
+++ 02_PlayerProfile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PlayerProfile
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Player Profile
+      string playerName = "Trevor";       //text
+      int level = 15;                    //whole number
+      double health = 80.7;             //decimal number
+      bool isInPoliceChase = true;     //true false
+      char rank = 'A';                //single character
+
+      Console.WriteLine("Player: " + playerName);
+      Console.WriteLine("Level: " + level);
+      Console.WriteLine("Health: " + health);
+      Console.WriteLine("Is in Police Chase: " + isInPoliceChase);
+      Console.WriteLine("Rank: " + rank);
+
+
+      
+
+
+    }
+  }
+}
---
+++ 02_PlayerProfile/README.md
@@ -0,0 +1,7 @@
+# 02 - Player Profile
+
+**Key Concepts:**
+- Variables 
+- Data Types (string, int, double, bool, char)
+
+![Console Output](screenshot.png)
