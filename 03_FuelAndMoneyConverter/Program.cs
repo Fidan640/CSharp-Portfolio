33327b57b8209ad1e6d7fb20194480d19ed8f743
Fidan <fdashdamirova640@gmail.com>
2025-11-12 18:31:26 +0400

    Add Chapter 3: Fuel and Money Converter

--- 03_FuelAndMoneyConverter/Program.cs
+++ 03_FuelAndMoneyConverter/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FuelAndMoneyConverter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      float fuelInLiters = 12.5f;
+      float pricePerLiter = 3.75f;
+
+      int wholeFuel = Convert.ToInt32(fuelInLiters);
+      int roundedTotalCost = Convert.ToInt32(fuelInLiters * pricePerLiter);
+
+      Console.WriteLine("Fuel: " + wholeFuel);
+      Console.WriteLine("Total money required: $" + roundedTotalCost);
+      
+    }
+  }
+}
