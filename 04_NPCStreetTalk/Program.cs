755e736febe53eef5f4c5dde88a033a1f3b92f26
Fidan <fdashdamirova640@gmail.com>
2025-11-12 19:51:40 +0400

    Add Chapter 04: NPC Street Talk

---
+++ 04_NPCStreetTalk/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace NPCStreetTalk
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string playerQuestion;
+      string npcResponse;
+
+      Console.WriteLine("You meet an NPC on the street. What do you ask?");
+  
+      //player question: Which car should I steal for my next mission?
+      playerQuestion = Console.ReadLine();
+
+      npcResponse = "Try the blue sports car down the alley, fast but low attention.";
+      Console.Write("NPC: " + npcResponse);
+
+    }
+  }
+}
---
+++ 04_NPCStreetTalk/README.md
@@ -0,0 +1,9 @@
+# 4 - NPC Street Talk
+Player ask a question, NPC reply is shown. 
+
+**Key Concepts:**
+- User Input
+- Console.WriteLine() for displaying user input
+- Console.ReadLine() for displaying output
+
+![Console Output](screenshot.png)
