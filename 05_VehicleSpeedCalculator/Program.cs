87ada7812b745815aa5da02a51ba9e038bafc7cb
Fidan <fdashdamirova640@gmail.com>
2025-11-14 19:53:29 +0400

    Add Chapter 5: Vehicle Speed Calculator

---
+++ 05_VehicleSpeedCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace VehicleSpeedCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double distance = 120; //meters
+      double time = 5; //seconds
+
+      double speed = distance / time; //arithmetic
+      speed += 7; //nitro boost //assignmnet
+
+      bool isFast = speed > 50; //comparisson
+      bool missionReady = (speed > 40 && speed < 120); //logical
+
+      Console.WriteLine(distance + " meters");
+      Console.WriteLine(time + " seconds");
+      Console.WriteLine(speed + " m/s");
+      Console.WriteLine("Is Fast: " + isFast );
+      Console.WriteLine("Mission ready: " + missionReady);
+
+
+
+      
+    }
+  }
+}
+
---
+++ 05_VehicleSpeedCalculator/README.md
@@ -0,0 +1,9 @@
+# 4 - Vehicle Speed Calculator
+Calculates vehicle speed and uses operators to check if the ride is fast and mission is ready.
+
+**Key Concepts**
+- Operators (Arithmetic, Assignment, Comparison, Logical)
+- Math
+
+![Console Output](screenshot.png)
+
