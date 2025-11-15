12e726faff2816a6423370857517a0f4ddee76c2
Fidan <fdashdamirova640@gmail.com>
2025-11-15 15:06:39 +0400

    Add Chapter 7: Police Chase Logic

---
+++ 07_PoliceChaseLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace PoliceChaseLogic
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Are you wanted by the police?");
+      string wantedInput = Console.ReadLine();
+
+      Console.WriteLine("Do you have a fast car?");
+      string carInput = Console.ReadLine();
+
+      if (wantedInput == "no")
+      {
+        Console.WriteLine("No chase today. You are free to cruise Console City!");
+      }
+      else if (wantedInput == "yes" && carInput == "yes")
+      {
+        Console.WriteLine("You escaped the cops through the highway!");
+      }
+      else if (wantedInput == "yes" && carInput == "no")
+      {
+        Console.WriteLine ("You got caught by the police!");
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please type 'yes' or 'no'.");
+      }
+
+    }
+  }
+}
---
+++ 07_PoliceChaseLogic/README.md
@@ -0,0 +1,9 @@
+# 07 - Police Chase Logic
+Simulates a police chase with four possible outcomes based on player choices.
+
+**Key Concepts:**
+- Boolean logic (yes/no)
+- If...Else Statements (if, else, else if)
+- User Input (Console.ReadLine())
+
+![Console Output](screenshot.png)
