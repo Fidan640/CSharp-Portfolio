e839c19d3747eebb37ed1b19c969ebd51c05de02
Fidan <fdashdamirova640@gmail.com>
2025-11-25 00:44:28 +0400

    Add Chapter 10: ATM Money Dispenser

---
+++ 10_ATMMoneyDispenser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ATMMoneyDispenser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int amountToWithdraw = 230;
+      
+      while (amountToWithdraw > 0)
+      {
+        int bill = 0;
+        if (amountToWithdraw >= 100) bill = 100;
+        else if (amountToWithdraw >= 50) bill = 50;
+        else if (amountToWithdraw >= 20) bill = 20;
+        else break;
+
+       amountToWithdraw -= bill;
+       Console.WriteLine("Dispensing $" + bill);
+      }
+
+         Console.WriteLine("ATM is empty! Time to run!");
+    }
+  }
+}
---
+++ 10_ATMMoneyDispenser/README.md
@@ -0,0 +1,9 @@
+# 10 - ATM Money Dispenser
+Simulates dispensing money in bills until the ATM is empty.
+
+**Key Concepts:**
+- Break statement
+- Loops (while)
+- If, Else
+
+![Console Output](screenshot.png)
