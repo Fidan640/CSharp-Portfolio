68eeaef785bb7a7aa1a534f156e786c878b6e097
Fidan <fdashdamirova640@gmail.com>
2025-11-24 22:22:29 +0400

    Add Chapter 9: Street Race Countdown

---
+++ 09_StreetRaceCountdown/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace StreetRaceCountdown
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 3; i > 0; i--)
+      {
+        Console.WriteLine(i + "...");
+      }
+      Console.WriteLine("GO!");
+
+      Console.WriteLine("Press Enter to start the race!");
+      Console.ReadLine();
+
+      for (int lap = 1; lap <= 2; lap++)
+      {
+        int racer = 1;
+        while (racer <= 3)
+        {
+        Console.WriteLine("Racer " + racer + " finished lap " + lap);
+        racer++;
+        }
+        Console.WriteLine();
+
+      }
+      Console.WriteLine("🏆 Race finished!");
+
+
+
+      
+        
+
+
+        
+
+    }
+  }
+}
---
+++ 09_StreetRaceCountdown/README.md
@@ -0,0 +1,9 @@
+# 9 - Street Race Countdown
+Simulates a race countdown and racer progress
+
+**Key Concepts:**
+- for loops
+- while loops
+- nested loops
+
+![Console Output](screenshot.png)
