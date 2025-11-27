1d2c06e81c800c3da51d77d2802505d833ff2e2f
Fidan <fdashdamirova640@gmail.com>
2025-11-27 23:55:15 +0400

    Add Chapter 11: Wanted Level System

---
+++ 11_WantedLevelSystem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace WantedLevelSystem
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] wantedLevel = {"★ Just a warning", 
+                             "★★ Patrol car nearby", 
+                             "★★★ SWAT called in",  
+                             "★★★★ Helicopters inbound", 
+                             "★★★★★ Full lockdown!" };
+     
+     foreach (string i in wantedLevel)
+      {
+        Console.WriteLine(i);
+      }
+
+      string[,] cityZones = {
+                             {"Downtown", "★★★★★"}, 
+                             {"Vinewood", "★★★" },
+                             {"Sandy Shores", "★"}
+                            };
+
+      for (int i = 0; i < cityZones.GetLength(0); i++)
+      {
+        Console.WriteLine(cityZones[i, 0] + " - " + cityZones[i, 1]);
+      }                  
+
+
+
+    }
+  }
+}
---
+++ 11_WantedLevelSystem/README.md
@@ -0,0 +1,9 @@
+# 11 - Wanted Level System
+Displays wanted levels using arrays, loops, and a 2D array for city zones.
+
+**Key Concept:**
+- Arrays
+- Loop through an array (foreach and for)
+- Multidimensional arrays
+
+![Console Output](screenshot.png)
